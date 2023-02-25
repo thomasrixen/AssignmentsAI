cbfd4dbedd4696f7678d927e612fbcfe4813b5e4
Thomas Rixen <thomas.rixen@student.uclouvain.be>
2023-02-25 12:06:49 +0100

    first try actions

--- Assignment-1-2023-20230221/tower_sorting.py
+++ Assignment-1-2023-20230221/tower_sorting.py
@@ -18,13 +18,43 @@ from search import *
 class TowerSorting(Problem):
 
     # Define methods specific to this problem
+
     def actions(self, state):
+        """Return the actions that can be executed in the given
+        state. The result would typically be a list, but if there are
+        many actions, consider yielding them one at a time in an
+        iterator, rather than building them all at once."""
+        indexFullCol = []
+        allActions = []
+
+        for i in range(state.number):
+            if len(state.grid[i]) == state.size:
+                indexFullCol.append(i)
+        nbrFullCol = len(indexFullCol)
+
+        for i in range(state.number):
+            for j in range(state.number):
+                if (j != i and j not in indexFullCol):
+                    allActions.append((i,j))
         
 
+        return allActions
+
+
     def result(self, state, action):
+        """Return the state that results from executing the given
+        action in the given state. The action must be one of
+        self.actions(state)."""
         pass
 
+
+
+
     def goal_test(self, state):
+        """Return True if the state is a goal. The default method compares the
+        state to self.goal or checks for state in self.goal if it is a
+        list, as specified in the constructor. Override this method if
+        checking against a single self.goal is not enough."""
         pass
 
 
@@ -56,7 +86,7 @@ class State:
 
     # Define how to test if two State objects are equal
     def __eq__(self, other):
-        
+        pass
 
     # Define how to hash a State object
     def __hash__(self):
@@ -85,8 +115,13 @@ def read_instance_file(filepath):
 
     return number_tower, size_tower, initial_grid
 
+state1 = read_instance_file("./instances/i01")
+State1 = State(state1[0], state1[1], state1[2])
+myAgent = TowerSorting(State1)
+print(state1)
+print(myAgent.actions(State1))
 
-if __name__ == "__main__":
+"""if __name__ == "__main__":
     # Check if the instance file has been provided
     if len(sys.argv) != 2:
         print(f"Usage: ./sort_tower.py <path_to_instance_file>")
@@ -116,4 +151,4 @@ if __name__ == "__main__":
     print("* Execution time:\t", str(end_timer - start_timer))
     print("* Path cost to goal:\t", node.depth, "moves")
     print("* #Nodes explored:\t", nb_exp)
-    print("* Queue size at goal:\t",  remaining_nodes)
+    print("* Queue size at goal:\t",  remaining_nodes)"""
